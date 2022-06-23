421fa25b49a964a10b5fcb133cd2fc943e512ae2
Farshid-997 <arnob.farshid@gmail.com>
2022-06-23 13:09:34 +0600

    edit updated

--- src/Component/SingleTodos.tsx
+++ src/Component/SingleTodos.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Todo } from './model'
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -11,7 +11,8 @@ type Props={
 
 
 const SingleTodos = ({todo,todos,setTodos}:Props) => {
-
+const [edit,setEdit]=useState<boolean>(false)
+const [editTodo,setEditTodo]=useState<string>(todo.todo)
   const handleDelete=(id:number)=>{
 setTodos(
   todos.filter((todo)=>todo.id!==id)
@@ -23,11 +24,39 @@ const handleDone=(id:number)=>{
     todos.map((todo)=>todo.id===id?{...todo,isDone:!todo.isDone}:todo)
   )
   }
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
+  
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
   return (
     <div>
-      <form className="todos__single">
+      <form className="todos__single" onSubmit={(e)=>handleEdit(e,todo.id)}>
 
-        {
+ {edit ? (
+  <input
+    value={editTodo}
+    onChange={(e) => setEditTodo(e.target.value)}
+    className="todos__single--text"
+    ref={inputRef}
+  />
+) : todo.isDone ? (
+  <s className="todos__single--text">{todo.todo}</s>
+) : (
+  <span className="todos__single--text">{todo.todo}</span>
+)}
+
+        {/* {
           todo.isDone?(
             <s  className="todos__single--text">
             {todo.todo}
@@ -37,11 +66,15 @@ const handleDone=(id:number)=>{
             {todo.todo}
                     </span>
           )
-        }
+        } */}
       
 
         <div>
-            <span
+            <span onClick={()=>{
+              if(!edit && !todo.isDone){
+                setEdit(!edit);
+              }
+            }} 
               className="icon"
             
             >
